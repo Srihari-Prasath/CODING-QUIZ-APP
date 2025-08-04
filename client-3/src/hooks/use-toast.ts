ab593cbe40343ac7c59d9ccab44a17b2f6d37243
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-08-04 09:47:22 +0530

    remove client

--- client-3/src/hooks/use-toast.ts
+++
@@ -1,9 +0,0 @@
-import toast from 'react-hot-toast';
-
-export function useToast() {
-  return {
-    toast: ({ title, description }: { title: string; description: string }) => {
-      toast.success(`${title}\n${description}`);
-    },
-  };
-}
