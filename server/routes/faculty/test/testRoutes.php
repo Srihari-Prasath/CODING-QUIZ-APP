8489503ba956f250495358e71c0c655f460dbda7
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-08-21 07:57:38 +0530

    Update test routes logic

--- server/routes/faculty/test/testRoutes.php
+++ server/routes/faculty/test/testRoutes.php
@@ -1,13 +1,9 @@
 <?php 
 
-require_once '../../../controllers/faculty/test/testController.php';
+require_once('../../../controllers/faculty/test/testcontroller.php');
 
 header('Content-Type: application/json');
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 
 $controller = new TestController();
