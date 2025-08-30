03f85db3b99f3e8655f875c97852bdc72d6faf93
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-08-30 09:55:47 +0530

    update home page

--- index.php
+++ index.php
@@ -1,23 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IQ Arena | NSCET</title>
     <script src="https://cdn.tailwindcss.com"></script>
-
-
     <link rel="stylesheet" href="./assets/css/resource/style.css">
     <link rel="stylesheet" href="./assets/css/home/style.css">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow-x: hidden;
+        }
+        #confetti {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            pointer-events: none;
+            z-index: 50;
+        }
+        .main-footer{
+            position: absolute;
+            width: 100%;
+            bottom: 0;
+        }
+        .top-img{
+             position: absolute;
+             left: 43%;
+             top: 18%;
+        }
+       
+        .main-sub-text{
+            margin-top: -5%;
+            z-index: 99;
+        }
+        .words{
+           margin-left: -5%;
+        }
+        
+    </style>
+
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
 </head>
+<body class="min-h-screen flex flex-col font-sans relative">
+    <canvas id="confetti"></canvas>
+     
+    <div class="w-full text-center py-4 bg-gradient-to-r from-orange-600 via-orange-500 to-orange-700 text-white font-bold text-lg md:text-2xl shadow-md z-20 relative">
+        THENI MELAPETTAI HINDU NADARGAL URAVINMURAI <br>
+        NADAR SARASWATHI COLLEGE OF ENGINEERING AND TECHNOLOGY
+        <div style="position: absolute;left:30px;top:0;width:140px;height:80px">
+            <img src="./assets/img/logo/logo.png" alt="">
+        </div>
+    </div>
 
-<body >
-    <?php include('./development_team.php') ?>
-   
+
+    <!-- Floating shapes -->
+    <div class="floating-shapes">
+        <div class="shape"></div>
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    
+    <div class="top-img">
+        <img src="./assets/img/logo/iqarena.png" alt="" width="190px" >
+    </div>
+
+    <header class="words w-full text-center main-text   py-16 relative z-10 mt-40 ">
+        
+        <div class="text">
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="100"><div class="letter">I</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="200"><div class="letter">Q</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="300"><div class="letter"></div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="400"><div class="letter">A</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="500"><div class="letter">R</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="600"><div class="letter">E</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="700"><div class="letter">N</div></div>
+            <div class="wrapper"  data-aos="fade-down-right"  data-aos-delay="800"><div class="letter">A</div></div>
+        </div>
+    </header>
+    
+    <p class="text-center font-bold text-orange-500 main-sub-text text-4xl -py-100 text-2xl">
+        "The Skill Engine for the Next-Gen Engineers"
+    </p>
 
 
+
+<main class="flex flex-col items-center justify-center flex-grow w-full max-w-6xl px-4 mx-auto relative z-10">
+        <div class="flex flex-col sm:flex-row gap-6 justify-center" >
+            <a href="./login.php" class="btn-primary text-white font-semibold py-4 px-8 rounded-xl text-xl transform transition-all duration-300">
+                Portal
+            </a>
+        </div>
+    </main> 
+
+
+    <div class="mt-10 main-footer">
+        <?php include('./resource/footer.php') ?>
+    </div>
+
+    
+
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 </body>
-
 </html>
